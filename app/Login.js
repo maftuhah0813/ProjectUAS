6d2c58a6f591b14116e414b973d2281dddf79530
maftuhah0813 <mmaftuhah44@gmail.com>
2025-06-24 10:34:16 +0700

    menambah file

--- app/Login.js
+++ app/Login.js
@@ -21,7 +21,7 @@ export default function Login() {
 
   return (
     <LinearGradient colors={['#6FB1FC', '#4364F7']} style={styles.container}>
-      <Image source={require('../assets/images/logo_alquran.png')} style={styles.logo} />
+      <Image source={require('../assets/images/logo_alquran.jpg')} style={styles.logo} />
       <Text style={styles.title}>Aplikasi Al-Qur'an</Text>
 
       <TextInput
